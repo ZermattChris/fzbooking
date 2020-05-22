40b843d4c2c5dae4deee6e608e5f877a4e496759
ZermattChris <chris@flyzermatt.com>
2020-05-22 15:50:15 +0200

    Added a basic "Scroll to top" button in footer

---
+++ src/components/FormSlider.tsx
@@ -0,0 +1,15 @@
+import { IonGrid, IonRow, IonCol} from '@ionic/react';
+import React from 'react';
+import './FormSlider.css';
+
+interface ContainerProps { }
+
+const FormSlider: React.FC<ContainerProps> = () => {
+  return (
+    <div id="main-contents" className="big-vertical-box">content goes here...</div>
+  );
+};
+
+
+
+export default FormSlider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
-import { IonGrid, IonRow, IonCol, IonContent, IonHeader, IonFooter, IonPage, IonTitle, IonToolbar, IonButtons, IonButton } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonContent, IonHeader, IonFooter, IonPage, IonTitle, IonToolbar, IonButtons, IonButton } from '@ionic/react';
+import React, {useRef} from 'react';
+import { add, settings, share, arrowUpCircleSharp, arrowUpOutline, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
+
+import FormSlider from '../components/FormSlider';
 import './Home.css';
 
-const Home: React.FC = () => {
+const Home: React.FC = (props) => {
+
+  const contentRef = useRef(null);
+  const scrollToTop = () => {
+      // @ts-ignore
+      contentRef.current.scrollToTop();
+  };
+
   return (
     <IonPage>
 
@@ -13,14 +22,19 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent>
+      <IonContent scrollEvents={true} ref={contentRef}>
         <IonGrid class="ion-no-padding">
           <IonRow>
             <IonCol size="2" className="maroon ion-hide-md-down">
               left
            </IonCol>
             <IonCol size-md="8" size="12">
-              <ExploreContainer />
+
+
+              {/* ------------- This is our dynamic multi-step form ------------- */}
+              <FormSlider />
+
+
             </IonCol>
             <IonCol size="2" className="maroon ion-hide-md-down">
               right
@@ -46,9 +60,15 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonFooter>
 
-      
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton className="tweak-to-top-btn" title="Scroll to Top..." size="small"  onClick={()=>scrollToTop()}> 
+            <IonIcon icon={arrowUpOutline}/>
+          </IonFabButton>
+      </IonFab>
+
     </IonPage>
   );
 };
+
 
 export default Home;
