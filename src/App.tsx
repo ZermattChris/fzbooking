a50e2b4cbc2bbd2af9c0157f0c691352c8fc2b0e
ZermattChris <chris@flyzermatt.com>
2020-05-22 14:46:17 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/" component={Home} exact={true} />
+        <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
       </IonRouterOutlet>
     </IonReactRouter>
